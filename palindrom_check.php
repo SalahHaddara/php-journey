d9ea1db730c20333cdc074fb5e437931a53b204b
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-16 09:47:52 +0200

    add palindrom logic

--- palindrom_check.php
+++ palindrom_check.php
@@ -1,5 +1,10 @@
 <?php
 function isPalindrom($palindrom)
 {
-
+    for ($i = 1; $i < strlen($palindrom) / 2; $i++) {
+        if ($palindrom[$i] !== $palindrom[strlen($palindrom) - $i]) {
+            return false;
+        }
+        return true;
+    }
 }
