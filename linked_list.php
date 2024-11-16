1225af33bf39a4ca5457263e4db3df9917ef8675
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-16 13:14:05 +0200

    add append node function

--- linked_list.php
+++ linked_list.php
@@ -12,3 +12,26 @@ class Node
     }
 }
 
+class LinkedList
+{
+    public $head;
+
+    public function __construct()
+    {
+        $this->head = null;
+    }
+
+    public function append($value)
+    {
+        $newNode = new Node($value);
+        if ($this->head === null) {
+            $this->head = $newNode;
+        } else {
+            $current = $this->head;
+            while ($current->next !== null) {
+                $current = $current->next;
+            }
+            $current->next = $newNode;
+        }
+    }
+}
