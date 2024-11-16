601814a671bd716960482c75b1b84ba4db03c263
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-16 17:25:40 +0200

    add checkPassword

--- user.php
+++ user.php
@@ -13,4 +13,24 @@ class User
         $this->password = $password;
     }
 
+    public static function checkPassword(string $password)
+    {
+        if (strlen($password) < 12) {
+            return false;
+        }
+
+        if (!preg_match('/[A-Z]/', $password)) {
+            return false;
+        }
+
+        if (!preg_match('/[a-z]/', $password)) {
+            return false;
+        }
+
+        if (!preg_match('/[^a-zA-Z0-9]/', $password)) {
+            return false;
+        }
+
+        return true;
+    }
 }
