0869913ae0fce9bcc4f87a8daabb54c190cf9baa
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-16 12:20:20 +0200

    add merge function

--- merge_sort.php
+++ merge_sort.php
@@ -13,4 +13,24 @@ function mergeSort($arr)
     $left = mergeSort($left);
     $right = mergeSort($right);
 
+    return merge($left, $right);
+}
+
+function merge($left, $right)
+{
+    $result = [];
+    $i = 0;
+    $j = 0;
+
+    while ($i < count($left) && $j < count($right)) {
+        if ($left[$i] < $right[$j]) {
+            $result[] = $left[$i];
+            $i++;
+        } else {
+            $result[] = $right[$j];
+            $j++;
+        }
+    }
+
+    return array_merge($result, array_slice($left, $i), array_slice($right, $j));
 }
